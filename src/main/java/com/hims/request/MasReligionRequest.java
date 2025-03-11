a08d1cccb8494df038dbaa12ea7c290833467312
Soubhagya <soubhagyaranjanpani125@gmail.com>
2025-03-11 15:25:30 +0530

    fix request

--- src/main/java/com/hims/request/MasReligionRequest.java
+++ src/main/java/com/hims/request/MasReligionRequest.java
@@ -6,8 +6,8 @@ import java.time.Instant;
 
 @Data
 public class MasReligionRequest {
-    private String name;         // Name of the religion
-    private String status;       // Status of the religion ("Y" for active, "N" for inactive)
-    private String lastChgBy;   // User who last changed the record
-    private Instant lastChgDate; // Timestamp of the last change
+    private String name;
+    private String status;
+    private String lastChgBy;
+
 }
