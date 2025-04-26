58f4be4bbbfef007d759fd19590e7585264d16d6
isharma1576 <isharma9075@gmail.com>
2025-04-26 22:52:49 +0530

    mas_main_chargecode completed

--- src/main/java/com/hims/service/impl/MasMainChargeCodeServicesImpl.java
+++ src/main/java/com/hims/service/impl/MasMainChargeCodeServicesImpl.java
@@ -2,21 +2,26 @@ package com.hims.service.impl;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.hims.entity.MasMainChargeCode;
-import com.hims.entity.MasOpdSession;
+import com.hims.entity.User;
 import com.hims.entity.repository.MasMainChargeCodeRepository;
+import com.hims.entity.repository.UserRepo;
 import com.hims.request.MasMainChargeCodeRequest;
 import com.hims.response.ApiResponse;
+import com.hims.response.DgUomResponse;
 import com.hims.response.MasMainChargeCodeDTO;
-import com.hims.response.MasOpdSessionResponse;
 import com.hims.service.MasMainChargeCodeService;
 import com.hims.utils.ResponseUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.Instant;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -30,9 +35,35 @@ public class MasMainChargeCodeServicesImpl implements MasMainChargeCodeService {
     @Autowired
     MasMainChargeCodeRepository masMainChargeCodeRepository;
 
-//    only use this and the commented part when need to connect department id from the table
-//    @Autowired
-//    MasDepartmentRepository departmentRepository;
+    @Autowired
+    UserRepo userRepo;
+    private String getCurrentTimeFormatted() {
+
+        return LocalTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss"));
+    }
+
+    private MasMainChargeCodeDTO toResponse(MasMainChargeCode code) {
+        MasMainChargeCodeDTO dto = new MasMainChargeCodeDTO();
+        dto.setChargecodeId(code.getChargecodeId());
+        dto.setChargecodeCode(code.getChargecodeCode());
+        dto.setChargecodeName(code.getChargecodeName());
+        dto.setStatus(code.getStatus());
+        dto.setLastChgBy(code.getLastChgBy());
+        dto.setLastChgDate(code.getLastChgDate());
+        dto.setLastChgTime(code.getLastChgTime());
+
+        return dto;
+    }
+
+    private User getCurrentUser() {
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        User user = userRepo.findByUserName(username);
+        if (user == null) {
+            log.warn("User not found for username: {}", username);
+        }
+        return user;
+    }
+
 
     @Override
     public ApiResponse<List<MasMainChargeCodeDTO>> getAllChargeCode(int flag) {
@@ -53,18 +84,6 @@ public class MasMainChargeCodeServicesImpl implements MasMainChargeCodeService {
         return ResponseUtils.createSuccessResponse(codeDTOS, new TypeReference<>() {});
     }
 
-    private MasMainChargeCodeDTO toResponse(MasMainChargeCode code) {
-        MasMainChargeCodeDTO dto = new MasMainChargeCodeDTO();
-        dto.setChargecodeId(code.getChargecodeId());
-        dto.setChargecodeCode(code.getChargecodeCode());
-        dto.setChargecodeName(code.getChargecodeName());
-        dto.setStatus(code.getStatus());
-        dto.setLastChgBy(code.getLastChgBy());
-        dto.setLastChgDate(code.getLastChgDate());
-        dto.setLastChgTime(code.getLastChgTime());
-
-        return dto;
-    }
 
     @Override
     public ApiResponse<MasMainChargeCodeDTO> getChargeCodeById(Long chargecodeId) {
@@ -82,16 +101,31 @@ public class MasMainChargeCodeServicesImpl implements MasMainChargeCodeService {
         return null;
     }
 
+
     @Override
     public ApiResponse<MasMainChargeCodeDTO> createChargeCode(MasMainChargeCodeRequest codeRequest) {
         MasMainChargeCode chargeCode = new MasMainChargeCode();
-//
-        chargeCode.setChargecodeCode(codeRequest.getChargecode_code());
-        chargeCode.setChargecodeName(codeRequest.getChargecode_name());
 
-        MasMainChargeCode mainCode = masMainChargeCodeRepository.save(chargeCode);
-        return ResponseUtils.createSuccessResponse(toResponse(mainCode), new TypeReference<MasMainChargeCodeDTO>() {});
+        if (!("y".equalsIgnoreCase(codeRequest.getStatus()) || "n".equalsIgnoreCase(codeRequest.getStatus()))) {
+            return ResponseUtils.createFailureResponse(null, new TypeReference<>() {
+            }, "Invalid status. Status should be 'Y' or 'N'", 400);
+        }
+        else{
+            chargeCode.setChargecodeCode(codeRequest.getChargecode_code());
+            chargeCode.setChargecodeName(codeRequest.getChargecode_name());
+            chargeCode.setStatus("y");
+            User currentUser = getCurrentUser();
+            if (currentUser == null) {
+                return ResponseUtils.createFailureResponse(null, new TypeReference<>() {},
+                        "Current user not found", HttpStatus.UNAUTHORIZED.value());
+            }
+            chargeCode.setLastChgBy(currentUser.getUsername());
+            chargeCode.setLastChgDate(Instant.now());
+            chargeCode.setLastChgTime(getCurrentTimeFormatted());
+            return ResponseUtils.createSuccessResponse(toResponse(masMainChargeCodeRepository.save(chargeCode)), new TypeReference<>() {});
+        }
     }
+
 
     @Override
     public ApiResponse<MasMainChargeCodeDTO> updateChargeCode(Long chargecodeId, MasMainChargeCodeRequest codeRequest) {
@@ -99,13 +133,28 @@ public class MasMainChargeCodeServicesImpl implements MasMainChargeCodeService {
 
         if(optionalCode.isPresent()){
             MasMainChargeCode chargeCode = optionalCode.get();
+            if("y".equals(codeRequest.getStatus())||"n".equals(codeRequest.getStatus())) {
+                chargeCode.setStatus(codeRequest.getStatus());
+            } else{
+                return ResponseUtils.createFailureResponse(null, new TypeReference<>() {
+                }, "Invalid status. Status should be 'y' or 'n'", 400);
+            }
             chargeCode.setChargecodeCode(codeRequest.getChargecode_code());
             chargeCode.setChargecodeName(codeRequest.getChargecode_name());
+            User currentUser = getCurrentUser();
+            if (currentUser == null) {
+                return ResponseUtils.createFailureResponse(null, new TypeReference<>() {},
+                        "Current user not found", HttpStatus.UNAUTHORIZED.value());
+            }
+            chargeCode.setLastChgBy(currentUser.getUsername());
+            chargeCode.setLastChgDate(Instant.now());
+            chargeCode.setLastChgTime(getCurrentTimeFormatted());
 
-            masMainChargeCodeRepository.save(chargeCode);
-            return ResponseUtils.createSuccessResponse(toResponse(chargeCode), new TypeReference<MasMainChargeCodeDTO>() {});
+            return ResponseUtils.createSuccessResponse(toResponse(masMainChargeCodeRepository.save(chargeCode)), new TypeReference<>() {});
         }
-        return ResponseUtils.createNotFoundResponse("Session not found", HttpStatus.NOT_FOUND.value());
+        else{
+            return ResponseUtils.createFailureResponse(null, new TypeReference<MasMainChargeCodeDTO>() {}, "FgUom data not found", 404);
+        }
     }
 
     @Override
