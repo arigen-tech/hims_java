3fe9e0bc4b034e6020613490b0fcaebf655c42d5
abhinav singha <abhinavsingha@outlook.com>
2025-04-30 14:24:57 +0530

    patient search and follow up appointment and registration changes
    (cherry picked from commit e5db25272005a9124a30923ca8a8db2f2909c12c)

--- src/main/java/com/hims/response/PatientRegFollowUpResp.java
+++ src/main/java/com/hims/response/PatientRegFollowUpResp.java
@@ -3,9 +3,11 @@ package com.hims.response;
 import com.hims.entity.OpdPatientDetail;
 import com.hims.entity.Patient;
 import com.hims.entity.Visit;
+import lombok.Getter;
 import lombok.Setter;
 
 @Setter
+@Getter
 public class PatientRegFollowUpResp {
     private Patient patient;
     private OpdPatientDetail opdPatientDetail;
