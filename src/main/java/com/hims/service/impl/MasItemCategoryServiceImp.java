f429b06452a8588ffe405e30c9af5dbda91bd046
Devansh Gangwar <gangwardevansh712@gmail.com>
2025-06-16 17:19:58 +0530

    small change

--- src/main/java/com/hims/service/impl/MasItemCategoryServiceImp.java
+++ src/main/java/com/hims/service/impl/MasItemCategoryServiceImp.java
@@ -158,7 +158,7 @@ private MasItemCategoryRepository masItemCategoryRepository;
             masItemCategory1.setLastChgBy(String.valueOf(currentUser.getUserId()));
             masItemCategory1.setLastChgDate(LocalDate.now());
             masItemCategory1.setLastChgTime(getCurrentTimeFormatted());
-            masItemCategory1.setStatus("y");
+
             if (masItemCategoryRequest.getSectionId() != null) {
                 Optional<MasStoreSection> masStoreSection = masStoreSectionRepository.findById(masItemCategoryRequest.getSectionId());
                 if (masStoreSection.isPresent()) {
