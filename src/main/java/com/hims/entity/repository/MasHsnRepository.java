2b5cbae309ff7dafd5eaa09c9aec22f41863d0fb
Devansh Gangwar <gangwardevansh712@gmail.com>
2025-06-26 14:51:14 +0530

    MasHsnReposi

--- src/main/java/com/hims/entity/repository/MasHsnRepository.java
+++ src/main/java/com/hims/entity/repository/MasHsnRepository.java
@@ -4,9 +4,12 @@ import com.hims.entity.MasHSN;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface MasHsnRepository extends JpaRepository<MasHSN,String> {
     List<MasHSN> findByStatusIgnoreCase(String y);
 
     List<MasHSN> findByStatusInIgnoreCase(List<String> y);
+
+    Optional<MasHSN> findByHsnCode(MasHSN hsnCode);
 }
