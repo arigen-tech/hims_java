4749bf4b5df9d515bce5835d432529b6e90c72b3
Soubhagya <soubhagyaranjanpani125@gmail.com>
2025-07-22 12:36:30 +0530

    complete searching api

---
+++ src/main/java/com/hims/request/PendingBillingSearchRequest.java
@@ -0,0 +1,11 @@
+package com.hims.request;
+
+import java.time.LocalDate;
+
+public class PendingBillingSearchRequest {
+
+    private String mobileNo;
+    private String patientName;
+    private String uhidNo;
+    private LocalDate appointmentDate;
+}
