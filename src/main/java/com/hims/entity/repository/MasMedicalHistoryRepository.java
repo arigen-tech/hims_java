965d1d24edcd8f53eaab5a6e1263b7a2d753e1a2
Devansh Gangwar <gangwardevansh712@gmail.com>
2025-12-08 10:07:23 +0530

    MasMedicalHistory api complete

--- src/main/java/com/hims/entity/repository/MasMedicalHistoryRepository.java
+++ src/main/java/com/hims/entity/repository/MasMedicalHistoryRepository.java
@@ -4,6 +4,11 @@ import com.hims.entity.MasMedicalHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MasMedicalHistoryRepository extends JpaRepository<MasMedicalHistory, Long> {
+    List<MasMedicalHistory> findByStatusIgnoreCaseIn(List<String> y);
+
+    List<MasMedicalHistory> findByStatusIgnoreCase(String y);
 }
