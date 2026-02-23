4e9067370cc7d9bd26de4337cfb7dfee7f30e2a2
Devansh Gangwar <gangwardevansh712@gmail.com>
2026-02-23 13:52:49 +0530

    complete RadiologyTemplate

---
+++ src/main/java/com/hims/response/RadiologyTemplateResponse.java
@@ -0,0 +1,17 @@
+package com.hims.response;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class RadiologyTemplateResponse {
+    private Long pacsTemplateId;
+    private String templateCode;
+    private String templateName;
+    private Long subChargecodeId;
+    private String templateText;
+
+}
