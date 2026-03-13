0e43bdc3b185fdd2dbc1ffd16013b201b954cdbc
Tushar <tushar.kumar20017@gmail.com>
2026-03-14 03:53:10 +0530

    need to merge

--- src/main/java/com/hims/entity/repository/BillingHeaderRepository.java
+++ src/main/java/com/hims/entity/repository/BillingHeaderRepository.java
@@ -11,9 +11,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import org.springframework.data.domain.Pageable;
-
-import java.awt.print.Pageable;
-import java.time.LocalDate;
 import java.util.List;
 
 public interface BillingHeaderRepository extends JpaRepository<BillingHeader, Integer> {
