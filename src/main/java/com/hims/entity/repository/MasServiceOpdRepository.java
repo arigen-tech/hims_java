922f05e2e844815e766166367ed7f2641c624ee1
Devansh Gangwar <gangwardevansh712@gmail.com>
2026-04-07 12:14:15 +0530

    masServiceOpdChanges complete

--- src/main/java/com/hims/entity/repository/MasServiceOpdRepository.java
+++ src/main/java/com/hims/entity/repository/MasServiceOpdRepository.java
@@ -1,6 +1,9 @@
 package com.hims.entity.repository;
 
 import com.hims.entity.*;
+import com.hims.projection.MasServiceOpdProjection;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -53,5 +56,69 @@ public interface MasServiceOpdRepository extends JpaRepository<MasServiceOpd, Lo
                                                  @Param("deptId") Long deptId,
                                                  @Param("serviceCat") MasServiceCategory serviceCat,
                                                  @Param("currentDateTime") Instant currentDateTime);
+
+
+    @Query(value = """
+    SELECT 
+        mso.id AS id,
+        mso.service_name AS serviceName,
+        mso.base_tariff AS baseTariff,
+        msc.service_cat_name AS serviceCategory,
+        ms.department_name AS departmentName,
+        me.emp_fn AS doctorFirstName,
+        me.emp_mn AS doctorMiddleName,
+        me.emp_ln AS doctorLastName,
+        mso.from_dt AS fromDate,
+        mso.to_dt AS toDate,
+        mso.status AS status
+    FROM mas_service_opd mso
+    LEFT JOIN mas_service_category msc 
+        ON mso.service_cat_id = msc.id
+    LEFT JOIN mas_department ms  
+        ON mso.department_id = ms.department_id
+    LEFT JOIN users u  
+        ON u.user_id = mso.doctor_id
+    LEFT JOIN mas_employee me  
+        ON u.employee_id = me.emp_id 
+    WHERE mso.hospital_id = :hospitalId
+    AND (:departmentId IS NULL OR mso.department_id = :departmentId)
+    AND (:doctorId IS NULL OR mso.doctor_id = :doctorId)
+    AND (
+        :doctorName IS NULL OR
+        LOWER(CONCAT(
+            COALESCE(me.emp_fn,''), ' ',
+            COALESCE(me.emp_mn,''), ' ',
+            COALESCE(me.emp_ln,'')
+        )) LIKE LOWER(CONCAT('%', :doctorName, '%'))
+    )
+
+    ORDER BY ms.department_name ASC
+""",
+            countQuery = """
+    SELECT COUNT(*) 
+    FROM mas_service_opd mso
+    LEFT JOIN users u ON u.user_id = mso.doctor_id
+    LEFT JOIN mas_employee me ON u.employee_id = me.emp_id
+    WHERE mso.hospital_id = :hospitalId
+    AND (:departmentId IS NULL OR mso.department_id = :departmentId)
+    AND (:doctorId IS NULL OR mso.doctor_id = :doctorId)
+    
+    AND (
+        :doctorName IS NULL OR
+        LOWER(CONCAT(
+            COALESCE(me.emp_fn,''), ' ',
+            COALESCE(me.emp_mn,''), ' ',
+            COALESCE(me.emp_ln,'')
+        )) LIKE LOWER(CONCAT('%', :doctorName, '%'))
+    )
+""",
+            nativeQuery = true)
+    Page<MasServiceOpdProjection> getOpdTariffByDepartmentAndDoctor(
+            @Param("hospitalId") Long hospitalId,
+            @Param("departmentId") Long departmentId,
+            @Param("doctorId") Long doctorId,
+            @Param("doctorName") String doctorName,
+            Pageable pageable
+    );
 }
 
