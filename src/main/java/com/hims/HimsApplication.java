d5b370ef11884a9f08ebb948028c12aa61cfb501
Vinay Vastrakar <101122381+VinayVastrakar@users.noreply.github.com>
2026-07-10 09:38:43 +0530

    chore:added logger (#168)

--- src/main/java/com/hims/HimsApplication.java
+++ src/main/java/com/hims/HimsApplication.java
@@ -1,6 +1,7 @@
 package com.hims;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -9,11 +10,13 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 @SpringBootApplication
 @EntityScan
+@Slf4j
 public class HimsApplication extends SpringBootServletInitializer {
 
 
 	public static void main(String[] args) {
 		SpringApplication.run(HimsApplication.class, args);
+		log.info("========== HIMS STARTED SUCCESSFULLY ==========");
 	}
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
