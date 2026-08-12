c88ef65776d95eb3a203b1e3f821ec35102d844a
Devansh <gangwardevansh712@gmail.com>
2026-08-12 12:11:17 +0530

    Ipd/work (#256)
    * saveProcedureConsumableTemplate and
    getProcedureConsumableTemplate and
    getProcedureConsumableTemplateDetails api complete and some changes complete
    
    * saveNursingCareProcedure and complete some changes
    
    * conflict resolve
    
    * getNursingCareProcedure api complete and call ledger utils

--- src/main/java/com/hims/entity/repository/IpConsumableTxnRepository.java
+++ src/main/java/com/hims/entity/repository/IpConsumableTxnRepository.java
@@ -1,9 +1,33 @@
 package com.hims.entity.repository;
 
 import com.hims.entity.IpConsumableTxn;
+import com.hims.projection.NursingCareProcedureProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface IpConsumableTxnRepository extends JpaRepository<IpConsumableTxn,Long> {
+
+    @Query("""
+            SELECT
+                ict.itemId.itemId AS itemId,
+                ict.itemName AS itemName,
+                ict.quantity AS qty,
+                ipt.procedureTxnId AS procedureTxnId,
+                ipt.procedureName AS procedureName,
+                ict.usageDatetime AS dateTime,
+                ict.usedBy AS usedBy,
+                ict.batchNo AS batchNo,
+                ict.expiryDate AS expiryDate,
+                ict.remarks AS remark
+            FROM IpConsumableTxn ict
+            LEFT JOIN ict.procedureTxnId ipt
+            WHERE ict.inpatientId.inpatientId = :inpatientId
+            ORDER BY ict.usageDatetime DESC, ict.consumableTxnId DESC
+            """)
+    List<NursingCareProcedureProjection> getNursingCareProcedureByInpatientId(@Param("inpatientId") Long inpatientId);
 }
--- src/main/java/com/hims/response/NursingCareProcedure.java
+++ src/main/java/com/hims/response/NursingCareProcedure.java
@@ -1,20 +1,23 @@
-package com.hims.request;
+package com.hims.response;
 
 import lombok.Data;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+
 @Data
-public class ConsumableEntryRequest {
+public class NursingCareProcedure {
     private Long itemId;
-    private Long InpatientId;
+    private String itemName;
+    private BigDecimal qty;
+    private Long procedureTxnId;
+    private String procedureName;
     private LocalDateTime dateTime;
-    private BigDecimal requestQty;
+    private String usedBy;
     private String batchNo;
     private LocalDate expiryDate;
-    private String givenBy;
     private String remark;
-    private Long procedureId;
+
 
 }
