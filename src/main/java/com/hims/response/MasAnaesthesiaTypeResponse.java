c7943eccc93c20f6c6714b9e1d22b2302d5dab57
Vinay Vastrakar <vinayvastrakar13@gmail.com>
2026-09-02 15:00:16 +0530

    feat:Added price in Anaesthesia master.

--- src/main/java/com/hims/response/MasAnaesthesiaTypeResponse.java
+++ src/main/java/com/hims/response/MasAnaesthesiaTypeResponse.java
@@ -3,6 +3,7 @@ package com.hims.response;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Data
@@ -12,6 +13,7 @@ public class MasAnaesthesiaTypeResponse {
     private Long anaesthesiaTypeId;
     private String anaesthesiaTypeCode;
     private String anaesthesiaTypeName;
+    private BigDecimal price;
     private String status;
     private String lastChgBy;
     private LocalDateTime lastChgDate;
