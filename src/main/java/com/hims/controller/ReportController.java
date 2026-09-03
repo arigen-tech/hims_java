8679e6bdc65c76f03592f0027992653589eb79c6
Vinay Vastrakar <vinayvastrakar13@gmail.com>
2026-09-03 17:22:09 +0530

    update with billHdId

--- src/main/java/com/hims/controller/ReportController.java
+++ src/main/java/com/hims/controller/ReportController.java
@@ -67,7 +67,7 @@ public class ReportController {
             @RequestParam Integer billHdId,
             @RequestParam String flag) {
         Map<String, Object> params = new HashMap<>();
-        params.put("Bill_header_id", billHdId);
+        params.put("Bill_hd_id", billHdId);
         params.put("path", Objects.requireNonNull(getClass().getResource(ReportConstants.ASSET_LOGO)).toString());
         try{
             if (ReportConstants.REPORT_FLAG_DOWNLOAD.equalsIgnoreCase(flag)) {
@@ -1840,7 +1840,7 @@ public class ReportController {
             @RequestParam Integer billHdId,
             @RequestParam String flag){
         Map<String , Object> params = new HashMap<>();
-        params.put("Bill_header_id", billHdId);
+        params.put("Bill_hd_id", billHdId);
         params.put("path", Objects.requireNonNull(getClass().getResource(ReportConstants.ASSET_LOGO)).toString());
 
         try{
