d2bcf7b47d76ebf103251298cd2c8fe17d43336e
DELL <gangwardevansh712@gmail.com>
2026-09-08 15:18:35 +0530

    getWardWiseInpatient api complete and some changes

---
+++ src/main/java/com/hims/response/WardWiseInpatientResponse.java
@@ -0,0 +1,24 @@
+package com.hims.response;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+@Data
+public class WardWiseInpatientResponse {
+    private Long inpatientId;
+    private String patientName;
+    private String uhid;
+    private String age;
+    private Long genderId;
+    private String gender;
+    private String mobileNo;
+    private String admissionNo;
+    private Long wardId;
+    private String ward;
+    private Long roomId;
+    private String room;
+    private Long bedId;
+    private String bed;
+    private LocalDateTime admissionDateTime;
+
+}
